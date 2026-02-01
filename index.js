96a408cf2185f8f35d2ef70c053dbf57bf0594d7
xebec19 <rohandeveloper106@gmail.com>
2026-02-01 16:06:25 +0530

    feat: added script to generate random quotes into assets as json

---
+++ index.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import https from "https";
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const QUOTE_URL =
+  "https://api.quotable.io/quotes/random?tags=inspirational&limit=100";
+const WRITE_FILE = path.join(__dirname + "/assets/quotes.json");
+
+// Create https agent that ignores SSL certificate errors (for development only)
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
+async function generateRandomeQuote(limit = 10) {
+  const start = process.hrtime.bigint();
+  try {
+    const response = await axios.request({ url: QUOTE_URL, httpsAgent });
+
+    const data = response.data;
+
+    await writeFile(WRITE_FILE, JSON.stringify(data, "", 2), { flag: "w" });
+  } catch (err) {
+    console.error("Request failed ", err);
+  } finally {
+    const end = process.hrtime.bigint();
+    const durationMs = Number(end - start) / 1e6;
+
+    console.log(`Took ${durationMs} ms`);
+  }
+}
+
+generateRandomeQuote(100);
